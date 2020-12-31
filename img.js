c077118e8a8241369b2141380ccb4a1ebff6aabe
lasurrealista <ladyblue@freemail.hu>
2020-12-31 18:56:35 +0100

    Slider HTML + CSS + JS (show image)

---
+++ img.js
@@ -0,0 +1,47 @@
+const img = [
+
+    {
+        id: 1,
+        title: "Vietnam",
+        source: "./img/Vietnam.jpg",
+        name: "Rice Fields in Vietnam",
+    },
+
+    {
+        id: 2,
+        title: "China",
+        source: "./img/Zhangye National Geopark-China.jpg",
+        name: "The Rainbow Mountains of China",
+    },
+
+    {
+        id: 3,
+        title: "Iceland",
+        source: "./img/Iceland.jpg",
+        name: "Northern Lights in Iceland",
+    },
+
+    {
+        id: 4,
+        title: "Indonesia",
+        source: "./img/Kelimutu-Indonesia.jpg",
+        name: "Tri-colored Lakes at Mount Kelimutu, Indonesia",
+    },
+
+    {
+        id: 5,
+        title: "USA",
+        source: "./img/Sedona-US.jpg",
+        name: "The Red Rocks of Sedona, USA",
+    },
+
+    {
+        id: 6,
+        title: "Kenya",
+        source: "./img/Amboseli National Park-Kenya.jpg",
+        name: "Amboseli National Park, Kenya",
+    },
+
+];
+
+export default img;
---
+++ slider.js
@@ -0,0 +1,56 @@
+'use strict';
+
+import img from './img.js';
+
+const nextButton = document.querySelector('.next');
+const previousButton = document.querySelector('.previous');
+const sliderContainer = document.querySelector('.slider__container');
+const dotContainer = document.querySelector('.dot__container');
+
+let numberOfImages = Object.keys(img).length;
+
+const getImage = (id, source, title, name) => {
+    const div = document.createElement('div');
+    div.className = 'slide';
+    div.innerHTML = `
+    <div class="number">${id} / ${numberOfImages}</div>
+    <img class="img__slide" src="${source}" 
+    alt="${title}">
+    <div class="name">${name}</div>`;
+    sliderContainer.appendChild(div);
+    return sliderContainer;
+};
+
+for (let i = 0; i < numberOfImages; i++) {
+    getImage(img[i].id, img[i].source, img[i].title, img[i].name);
+};
+
+const getDots = () => {
+    let elements = '';
+    for (let i = 0; i < numberOfImages; i++) {
+        elements += '<span class="dot"></span>';
+    };
+    dotContainer.innerHTML = elements;
+    dotContainer.style.display = 'block';
+    return dotContainer;
+};
+
+getDots();
+
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+let indexOfSlide = 0;
+
+const showImage = (index) => {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+            dots.forEach((dot) => {
+            dot.classList.remove('active');
+      })
+    });
+    slides[index].style.display = 'block';
+    dots[index].classList.add('active');
+  }
+
+showImage(indexOfSlide);
+
