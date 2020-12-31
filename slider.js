4ccff37ad4f05ebf3962dcbc64ae8d2a60198029
lasurrealista <ladyblue@freemail.hu>
2020-12-31 19:39:14 +0100

    Navigation (dots), automatic slideshow, resizing option.

--- slider.js
+++ slider.js
@@ -67,3 +67,21 @@ const previousSlide = () => {
 nextButton.addEventListener('click', nextSlide);
 previousButton.addEventListener('click', previousSlide);
 
+setInterval(() => {
+    nextSlide()
+  }, 4000);
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+      showImage(index);
+    })
+});
+
+const images = document.querySelectorAll('.img__slide');
+
+const resizeSlider = (height) => {
+    sliderContainer.style.height = height + 'px';
+    images.forEach(image => image.style.height = height + 'px');
+}
+
+resizeSlider(400);
